4bc4caa619de9c3e04905ba190904b51896a105b
bonveeee <bonochieng@gmail.com>
2022-06-15 09:15:53 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit' ,(e) => {
+    e.preventDefault()
+    newTodo(e.target.newtask.value)
+    form.reset()
+  })
 });
+function newTodo(task) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click' , deleteTodo)
+  btn.textContent = 'Done(X)'
+  li.textContent = `${task} `
+  li.appendChild(btn)
+  document.querySelector("#tasks").appendChild(li)
+}
+
+function deleteTodo(e){
+  e.target.parentNode.remove()
+}
